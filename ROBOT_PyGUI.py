32d4c6198e08e53fdc180edd6e608c5bbcef620f
Cameron More <123985185+cameronmore@users.noreply.github.com>
2023-08-13 12:19:46 -0400

    Cleaned up code

--- ROBOT_PyGUI.py
+++ ROBOT_PyGUI.py
@@ -3,10 +3,10 @@ import subprocess
 
 # Define the GUI layout
 layout = [
-    #[sg.Text("Enter a command to run:"), sg.InputText(key="input")],
     [sg.Text('Choose a command'),sg.DropDown(values=['query','verify', 'help'],key='command')],
     [sg.Text('Select an ontology'),sg.FileBrowse("Browse", key="ontology_browse")],
     [sg.Text('Select a query'),sg.FileBrowse("Browse", key="query_browse")],
+    [sg.Text('Select a place to store results'),sg.FileBrowse("Browse", key="result_loc")],
     [sg.Button("Run"), sg.Button("Cancel")],
     [sg.Output(size=(80, 10))]
 ]
@@ -24,10 +24,9 @@ while True:
     if event == "Run":
         if values['command']=='query':
             command='robot query --input ' + values['ontology_browse'] + ' --query ' + values['query_browse']
-        #Put in the hard coded inputs and outputs of each robot command (query, verify, filter, etc)
-        #command = 'robot ' + values["command"] + '' # + ' ' + values["input"] #Add dropdown menu to this line!!
         elif values['command']=='help':
             command = 'robot help'
+        #Hard code more robot commands here
 
 
         try:
